30eb572447f9267a2aba5f7287b7963339f94b22
adiapel <adiapel@cs.huji.ac.il>
2021-06-17 16:41:50 +0300

    activity_get_access and RequestAccessActivity is added

---
+++ app/src/main/java/communi/dog/aplicatiion/RequestAccessActivity.java
@@ -0,0 +1,14 @@
+package communi.dog.aplicatiion;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RequestAccessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_access);
+    }
+}
