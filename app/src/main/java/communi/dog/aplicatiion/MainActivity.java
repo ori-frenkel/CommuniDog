e6325f7f12f82f7cb50bb8b5c77ef9fd7b7fe1cb
Ido Sagiv <ido10796@gmail.com>
2021-06-21 21:40:55 +0300

    remember loged in users and add wait screen

--- app/src/main/java/communi/dog/aplicatiion/MainActivity.java
+++ app/src/main/java/communi/dog/aplicatiion/MainActivity.java
@@ -13,16 +13,23 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.wait_screen);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         updateUI(CommuniDogApp.getInstance().getDb().getUsersAuthenticator().getCurrentUser());
     }
 
     private void updateUI(FirebaseUser user) {
         if (user != null) {
-            startActivity(new Intent(this, MapScreenActivity.class));
+            DB db = CommuniDogApp.getInstance().getDb();
+            db.setCurrentUser(user);
+            db.currentUSerLiveData.observe(this, user1 -> {
+                startActivity(new Intent(this, MapScreenActivity.class));
+                finish();
+            });
+
         } else {
             startActivity(new Intent(this, LoginActivity.class));
+            finish();
         }
-        finish();
     }
 }
