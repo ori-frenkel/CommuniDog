1189c6f580ee1f3e013819b9d6814eab76247591
Ido Sagiv <ido10796@gmail.com>
2021-06-21 19:38:53 +0300

    check if user already logged in and update UI accordingly

--- app/src/main/java/communi/dog/aplicatiion/MainActivity.java
+++ app/src/main/java/communi/dog/aplicatiion/MainActivity.java
@@ -7,17 +7,34 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-        if (getIntent().getBooleanExtra("LOGOUT", false)) {
-            finish();
-            return;
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            startActivity(new Intent(this, MapScreenActivity.class));
+        } else {
+            startActivity(new Intent(this, LoginActivity.class));
         }
-        // todo: check if already logged in and open the relevant screen (login or map)
-        startActivity(new Intent(this, LoginActivity.class));
+        finish();
     }
 }
-
