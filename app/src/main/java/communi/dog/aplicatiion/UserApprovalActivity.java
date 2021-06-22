7d0e83a22b9f8a66cd419267ba681b18e7e15e75
Ido Sagiv <ido10796@gmail.com>
2021-06-22 16:41:05 +0300

    recyclerView in userApproval layout

--- app/src/main/java/communi/dog/aplicatiion/UserApprovalActivity.java
+++ app/src/main/java/communi/dog/aplicatiion/UserApprovalActivity.java
@@ -1,21 +1,11 @@
 package communi.dog.aplicatiion;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.text.Html;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import java.util.ArrayList;
-import java.util.Random;
 
 public class UserApprovalActivity extends AppCompatActivity {
     private Mail mailAccount;
@@ -27,7 +17,7 @@ public class UserApprovalActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notifications);
+        setContentView(R.layout.activity_users_approval);
 
         mailUserName = "userName";
         mailAccount = new Mail(mailUserName, "pass");
